ef9d39c4115a6f9931205263a8abcb41d63e1651
Albert Magyar <magyar@eecs.berkeley.edu>
2014-11-21 03:35:24 -0800

    Compiles to C++ emulator when connected to Rocket.

--- src/main/scala/hurroccane.scala
+++ src/main/scala/hurroccane.scala
@@ -8,7 +8,7 @@ object HurroccaneConstants {
                 "send" -> Bits("b0"),
                 "recv" -> Bits("b1")
                )
-  def params(param: Int) = { param }
+  def params(param: Int): Int = { param }
   val HurroccaneNumPorts: Int = 4
   val XprLen: Int = 64
 }
@@ -20,8 +20,6 @@ abstract trait UsesHurroccaneParameters extends UsesParameters {
 class Hurroccane(isLoopback: Boolean = true)
 extends rocket.RoCC with UsesHurroccaneParameters {
 
-  import HurroccaneConstants._ // until params() is working in project
-
   // Queue manager is the "core logic" of the hurroccane rocc queues
   val queueMgr = Module(new HurroccaneQueueMgr())
 
@@ -29,8 +27,8 @@ extends rocket.RoCC with UsesHurroccaneParameters {
   // Command shim logic
   queueMgr.io.cmd.valid := io.cmd.valid
   queueMgr.io.cmd.bits.op := io.cmd.bits.inst.funct(log2Up(HurroccaneConstants.ops.size)-1,0)
-  queueMgr.io.cmd.bits.recvDir := io.cmd.bits.rs1(log2Up(params(HurroccaneNumPorts))-1,0)
-  queueMgr.io.cmd.bits.sendMask := io.cmd.bits.rs1(params(HurroccaneNumPorts)-1,0)
+  queueMgr.io.cmd.bits.recvDir := io.cmd.bits.rs1(log2Up(HurroccaneConstants.HurroccaneNumPorts)-1,0)
+  queueMgr.io.cmd.bits.sendMask := io.cmd.bits.rs1(HurroccaneConstants.HurroccaneNumPorts-1,0)
   queueMgr.io.cmd.bits.sendData := io.cmd.bits.rs2
   io.cmd.ready := queueMgr.io.cmd.ready
 
@@ -44,12 +42,32 @@ extends rocket.RoCC with UsesHurroccaneParameters {
 
   // Loopback queues for testing
   if (isLoopback) {
-    val loopbackQueues = Vec.fill(params(HurroccaneNumPorts)) {
-      Queue(Bits(width = params(XprLen)), 2)
+    val loopbackQueues = Array.fill(HurroccaneConstants.HurroccaneNumPorts) {
+      Module(new Queue(Bits(width = HurroccaneConstants.XprLen), 2))
     }
-    for (i <- 0 until params(HurroccaneNumPorts)) {
-      queueMgr.io.ports(i).out <> loopbackQueues.io.enq
-      loopbackQueues.io.deq    <> queueMgr.io.ports(i).in
+    for (i <- 0 until HurroccaneConstants.HurroccaneNumPorts) {
+      loopbackQueues(i).io.enq <> queueMgr.io.ports(i).out
+      queueMgr.io.ports(i).in <> loopbackQueues(i).io.deq
     }
   }
+
+  
+  // Tie off unused outputs
+  io.busy := Bool(false)
+  io.interrupt := Bool(false)
+
+  io.mem.req.valid := Bool(false)
+  io.mem.req.bits.addr := Bits(0)
+  io.mem.req.bits.tag := Bits(0)
+  io.mem.req.bits.cmd := Bits(0)
+  io.mem.req.bits.typ := Bits(0)
+  io.mem.req.bits.data := Bits(0)
+
+  io.imem.acquire.valid := Bool(false)
+  io.imem.grant.ready := Bool(false)
+  io.imem.finish.valid := Bool(false)
+  io.iptw.req.valid := Bool(false)
+  io.dptw.req.valid := Bool(false)
+  io.pptw.req.valid := Bool(false)
+
 }
--- src/main/scala/queue-mgr.scala
+++ src/main/scala/queue-mgr.scala
@@ -5,23 +5,23 @@ import uncore._
 import HurroccaneConstants._ // until params() is working in project
 
 class HurroccanePort extends Bundle with UsesHurroccaneParameters {
-  val in = Decoupled(Bits(width = params(XprLen))).flip
-  val out = Decoupled(Bits(width = params(XprLen)))
+  val in = Decoupled(Bits(width = HurroccaneConstants.XprLen)).flip
+  val out = Decoupled(Bits(width = HurroccaneConstants.XprLen))
 }
 
 class HurroccaneQueueMgrCmd extends Bundle with UsesHurroccaneParameters {
   val op       = Bits(width = log2Up(HurroccaneConstants.ops.size))
-  val recvDir  = Bits(width = log2Up(params(HurroccaneNumPorts)))
-  val sendMask = Bits(width = log2Up(params(HurroccaneNumPorts)))
-  val sendData = Bits(width = params(XprLen))
+  val recvDir  = Bits(width = log2Up(HurroccaneConstants.HurroccaneNumPorts))
+  val sendMask = Bits(width = HurroccaneConstants.HurroccaneNumPorts)
+  val sendData = Bits(width = HurroccaneConstants.XprLen)
 }
 
 class HurroccaneQueueMgrResp extends Bundle with UsesHurroccaneParameters {
-  val recvData = Bits(width = params(XprLen))
+  val recvData = Bits(width = HurroccaneConstants.XprLen)
 }
 
 class HurroccaneQueueMgrIO extends Bundle with UsesHurroccaneParameters {
-  val ports = Vec.fill(params(HurroccaneNumPorts)){ new HurroccanePort() }
+  val ports = Vec.fill(HurroccaneConstants.HurroccaneNumPorts){ new HurroccanePort() }
   val cmd   = Decoupled(new HurroccaneQueueMgrCmd).flip
   val resp  = Decoupled(new HurroccaneQueueMgrResp)
 }
@@ -29,38 +29,38 @@ class HurroccaneQueueMgrIO extends Bundle with UsesHurroccaneParameters {
 class HurroccaneQueueMgr extends Module with UsesHurroccaneParameters {
   val io = new HurroccaneQueueMgrIO
 
-  val isRecv  = io.cmd.bits.op == HurroccaneConstants.ops("recv")
+  val isRecv  = io.cmd.bits.op === HurroccaneConstants.ops("recv")
   val inDir   = io.cmd.bits.recvDir
 
-  val isSend = io.cmd.bits.op == HurroccaneConstants.ops("send")
+  val isSend  = io.cmd.bits.op === HurroccaneConstants.ops("send")
   val outMask = Mux(isSend,
                     io.cmd.bits.sendMask,
                     Bits(0))
 
   // examine global readiness
-  val outPortClear = Vec.fill(params(HurroccaneNumPorts)){ Bool() }
-  for (i <- 0 until params(HurroccaneNumPorts)) {
-    outPortClear(i) = io.ports(i).out.ready || ~outMask(i)
+  val outPortClear = Vec.fill(HurroccaneConstants.HurroccaneNumPorts){ Bool() }
+  for (i <- 0 until HurroccaneConstants.HurroccaneNumPorts) {
+    outPortClear(i) := io.ports(i).out.ready || ~outMask(i)
   }
-  val inPortClear  = io.ports(inDir).in.ready || ~isRecv
-  val allClear = outPortClear.toBits().AndR && inPortClear
+  val inPortClear = io.ports(inDir).in.valid || ~isRecv
+  val allClear = ((~outPortClear.toBits()) === Bits(0)) && inPortClear
 
   // setup input ports
-  for (i <- 0 until params(HurroccaneNumPorts)) {
+  for (i <- 0 until HurroccaneConstants.HurroccaneNumPorts) {
     io.ports(i).in.ready := Bool(false)
   }
   io.ports(inDir).in.ready := allClear
 
   // setup output ports
-  for (i <- 0 until params(HurroccaneNumPorts)) {
-    val blockedOn = Vec.fill(params(HurroccaneNumPorts)){ Bool() }
+  for (i <- 0 until HurroccaneConstants.HurroccaneNumPorts) {
+    val blockedOn = Vec.fill(HurroccaneConstants.HurroccaneNumPorts){ Bool() }
     blockedOn(i) := Bool(false)
-    for (j <- 0 until params(HurroccaneNumPorts)) {
+    for (j <- 0 until HurroccaneConstants.HurroccaneNumPorts) {
       if (i != j) {
         blockedOn(j) := ~io.ports(j).out.ready && outMask(j)
       }
     }
-    io.ports(i).out.valid := (blockedOn.toBits() == Bits(0))
+    io.ports(i).out.valid := (blockedOn.toBits() === Bits(0))
     io.ports(i).out.bits := io.cmd.bits.sendData
   }
 
